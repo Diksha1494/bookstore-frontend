3b65575c1f65ec6857acc548b37b37c0fa9e8eaa
Diksha1494 <raidiksha0914@gmail.in>
2025-12-10 14:13:49 +0530

    change file name

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -12,7 +12,7 @@ import AdminRoute from "./AdminRoute";
 import AdminLogin from "../components/AdminLogin";
 import DashboardLayout from "../pages/dashboard/DashboardLayout";
 import Dashboard from "../pages/dashboard/Dashboard";
-import AddBook from "../pages/dashboard/addBook/AddBook";
+import AddBook from "../pages/dashboard/addbook/AddBook";
 import UpdateBook from "../pages/dashboard/EditBook/UpdateBook";
 import UserDashboard from "../pages/dashboard/users/UserDashboard";
 import ManageBooks from "../pages/dashboard/ManageBook/ManageBooks";
